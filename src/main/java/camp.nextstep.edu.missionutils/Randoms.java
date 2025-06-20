1479de57b1e3e31c694a08db8bd903231bdf691b
leeminho <tommymh97@naver.com>
2025-06-21 00:25:55 +0900

    javaLotto1.1

--- src/main/java/camp.nextstep.edu.missionutils/Randoms.java
+++ src/main/java/camp.nextstep.edu.missionutils/Randoms.java
@@ -1,14 +1,20 @@
 package camp.nextstep.edu.missionutils;
 
-import java.util.Random;
+import java.util.*;
 
 public class Randoms {
-    private static final Random RANDOM = new Random();
 
-    public static int pickNumberInRange(int startInclusive, int endInclusive) {
-        if (startInclusive > endInclusive) {
-            throw new IllegalArgumentException("startInclusive must be <= endInclusive");
+    public static List<Integer> pickUniqueNumbersInRange(int startInclusive, int endInclusive, int count) {
+        if (endInclusive - startInclusive + 1 < count) {
+            throw new IllegalArgumentException("[ERROR] 범위보다 많은 수를 뽑을 수 없습니다.");
         }
-        return RANDOM.nextInt(endInclusive - startInclusive + 1) + startInclusive;
+
+        List<Integer> candidates = new ArrayList<>();
+        for (int i = startInclusive; i <= endInclusive; i++) {
+            candidates.add(i);
+        }
+
+        Collections.shuffle(candidates);
+        return new ArrayList<>(candidates.subList(0, count));
     }
 }
